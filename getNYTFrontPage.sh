5f6f003151dd7611e4c74068109a423ea8521a12
Shawn Bierman <shawnbierman@icloud.com>
2020-05-31 11:37:20 -0500

    riots of George Floyd death

--- getNYTFrontPage.sh
+++ getNYTFrontPage.sh
@@ -6,6 +6,7 @@ year=`date +%Y`
 
 echo $month
 
+echo curl https://static01.nyt.com/images/$year/$month/$day/nytfrontpage/scan.pdf --output NYTimes-FrontPage-$month-$day-$year.pdf
 curl https://static01.nyt.com/images/$year/$month/$day/nytfrontpage/scan.pdf --output NYTimes-FrontPage-$month-$day-$year.pdf
 
 open NYTimes-FrontPage-$month-$day-$year.pdf
